b226a186407f8eafa4d62e2f650905eb5f8b935a
GideonMissoy <giddykips2030@gmail.com>
2024-05-28 14:35:13 +0300

    Hot fix.

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,12 +1,16 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    return {
-        [`income-${getCurrentYear()}`]: income,
-        [`gdp-${getCurrentYear()}`]: gdp,
-        [`capita-${getCurrentYear()}`]: capita,
-    };
+  const currentYear = getCurrentYear();
+
+  const budget = {
+    [`income-${currentYearurrentYear()}`]: income,
+    [`gdp-${currentYear()}`]: gdp,
+    [`capita-${currentYear()}`]: capita,
+  };
+
+  return budget;
 }
