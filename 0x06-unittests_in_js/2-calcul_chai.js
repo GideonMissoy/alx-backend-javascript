aaf19273a7511be3db68d83693829f41938f5aaf
GideonMissoy <giddykips2030@gmail.com>
2024-07-25 23:48:18 +0300

    Cleaning up.

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,23 +1,23 @@
 function calculateNumber(type, a, b) {
-    const an = Math.round(a);
-    const bn = Math.round(b);
-    let p = 0;
-    switch (type) {
+  const an = Math.round(a);
+  const bn = Math.round(b);
+  let p = 0;
+  switch (type) {
     case 'SUM':
-	p = an + bn;
-	break;
+      p = an + bn;
+      break;
     case 'SUBTRACT':
-	p = an - bn;
-	break;
+      p = an - bn;
+      break;
     case 'DIVIDE':
       if (bn === 0) {
-            p = "Error";
+        p = 'Error';
       } else {
-            p = an / bn;
+        p = an / bn;
       }
       break;
-    }
-    return p;
+  }
+  return p;
 }
 
 module.exports = calculateNumber;
