1f0deabf2bea6076d82af867ef5ae54a4f3773a5
GideonMissoy <giddykips2030@gmail.com>
2024-07-25 23:45:41 +0300

    3. Spies.

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+	const an = Math.round(a);
+	const bn = Math.round(b);
+	let p = 0;
+	switch (type) {
+	case 'SUM':
+            p = an + bn;
+            break;
+	case 'SUBTRACT':
+            p = an - bn;
+            break;
+	case 'DIVIDE':
+	    if (bn === 0) {
+		p = "Error";
+	    } else {
+		p = an / bn;
+	    }
+	    break;
+	}
+	return p;
+    }
+}
+
+module.exports = Utils;
